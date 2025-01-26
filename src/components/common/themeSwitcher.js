48b44ed1f4d1539a50a07e6bf333e542c4e65755
AkhilAjith2 <akhilajith2003@gmail.com>
2025-01-26 15:18:37 +0100

    deleted some comments

--- src/components/common/themeSwitcher.js
+++ src/components/common/themeSwitcher.js
@@ -1,34 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import '../pageStyles.css'; // Your theme styles (defines light and dark modes)
+import React, {useEffect, useState} from 'react';
 
 const ThemeSwitcher = () => {
-  // Set initial theme from localStorage or default to light
-  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
-  useEffect(() => {
-    // Apply theme to document element
-    document.documentElement.setAttribute('data-theme', theme);
-    // Save theme to localStorage
-    localStorage.setItem('theme', theme);
-  }, [theme]);
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
 
-  return (
-    <div className="theme-switcher-container">
-      <label className="switch">
-        <input
-          type="checkbox"
-          onChange={toggleTheme}
-          checked={theme === 'dark'}
-        />
-        <span className="slider"></span>
-      </label>
-      {/* <p>{theme === 'dark' ? '🌙 Dark Mode' : '☀️ Light Mode'}</p> */}
-    </div>
-  );
+    return (
+        <div className="theme-switcher-container">
+            <label className="switch">
+                <input
+                    type="checkbox"
+                    onChange={toggleTheme}
+                    checked={theme === 'dark'}
+                />
+                <span className="slider"></span>
+            </label>
+        </div>
+    );
 };
 
 export default ThemeSwitcher;
